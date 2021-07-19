f06f48b371afa40fa958c1ad47f629c1e85f1779
John M. Wargo <johnwargo@users.noreply.github.com>
2021-07-18 21:28:55 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-// import { CommaObjectPipe } from './pipes/comma-object.pipe';
 
 @NgModule({
   declarations: [AppComponent],
